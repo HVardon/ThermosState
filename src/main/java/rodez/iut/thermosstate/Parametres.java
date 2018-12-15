7bfbd75e198a356ac30d18fe7c1b1cf18fe28488
HVardon <hugo.vardon@iut-rodez.fr>
2018-12-15 19:48:16 +0100

    IHM terminé + Listener liste déroulante

--- src/main/java/rodez/iut/thermosstate/Parametres.java
+++ src/main/java/rodez/iut/thermosstate/Parametres.java
@@ -4,9 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class Parametres extends AppCompatActivity {
+    Spinner spinnerVal, spinnerValType; // liste déroulante pour valeurs et type de valeurs
 
     private Button retour;
     // intent pour accéder à l'accueil
@@ -20,7 +24,32 @@ public class Parametres extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parametres);
 
-        //methode permetant d'appliquer l'intent suite à un clic
+        spinnerVal = (Spinner) findViewById(R.id.spinnerVal);
+        spinnerValType = (Spinner) findViewById(R.id.spinnerValType);
+        // création d'un ArrayAdaptater permettant de changer l'arrayList d'un spinner, ici on lui attribu celle des secondes et minutes
+        final ArrayAdapter<CharSequence> ADAPTERSM =  ArrayAdapter.createFromResource(this,R.array.valeur_SecMin,android.R.layout.simple_spinner_item);
+        // création d'un ArrayAdaptater permettant de changer l'arrayList d'un spinner, ici on lui attribu celle des heures
+        final ArrayAdapter<CharSequence> ADAPTERH =  ArrayAdapter.createFromResource(this,R.array.valeur_heure,android.R.layout.simple_spinner_item);
+
+        //permet d'adapter la liste déroulante de valeur en fonction de celle du type
+        spinnerValType.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(spinnerValType.getSelectedItem().equals("Seconde(s)") || spinnerValType.getSelectedItem().equals("Minute(s)")){
+                    spinnerVal.setAdapter(ADAPTERSM);
+                } else if(spinnerValType.getSelectedItem().equals("Heure(s)")) {
+                    spinnerVal.setAdapter(ADAPTERH);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+        //methode permetant d'appliquer l'intent retour après un clic
         retour = findViewById(R.id.btn_Retour);
         retour.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,4 +58,5 @@ public class Parametres extends AppCompatActivity {
             }
         });
     }
+
 }
